910f393148901615fc9c123c32764acbfbccdabe
prateekkukreja <prateek.kukreja18@gmail.com>
2022-07-23 10:42:14 +0530

    Tests

---
+++ src/test/java/Logic1/special11.java
@@ -0,0 +1,17 @@
+package Logic1;
+
+public class special11 {
+    //    We'll say a number is special if it is a multiple of 11 or if it is one more than a multiple of 11.
+//    Return true if the given non-negative number is special. Use the % "mod" operator -- see Introduction to Mod
+//    specialEleven(22) → true
+    public static void main(String[] args) {
+        specialEleven(23);
+    }
+
+    public static boolean specialEleven(int n) {
+        if (n % 11 == 0 || n % 11 == 1) {
+            return true;
+        }
+        return false;
+    }
+}
